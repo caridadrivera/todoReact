f1a93dde93ac24d485e164ef4c04c458d95682d9
Caridad Rivera <ccrive10@gmail.com>
2020-06-05 15:00:11 -0400

    object is returned

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      currentItem:{
+        text: '',
+        key: ''
+      }
+    }
+    //when trying to type the list item, I got an error that this is undefined
+    //solution: bind 'this' to the constructor functions bc 'this' doesnt automatically point to the class.
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+
+  }
+
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+
+  addItem(e){
+    //prevent button's default behavior of refreshing
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem)
+  }
+
+
+
+  render() {
+    return (
+      <header>
+        <div className="App">
+          <form id="list-form" onSubmit={this.addItem}>
+              <input type="text" placeholder="Enter test"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}/>
+              <button type="submit"> Add </button>
+            </form>
+        </div>
+      </header>
+    )
+  }
+}
+export default App;
